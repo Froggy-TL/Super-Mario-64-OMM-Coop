e362c2ffb48e0838245dc78a0f36b26d29a260f7
Froggy-TL <Froggy-TL@users.noreply.github.com>
2026-08-16 20:35:41 -0400

    Remove OMM duplicate headers (use coopdx headers), simplify Android.mk includes

--- data/omm/engine/headers/src/goddard/renderer.h
+++
@@ -1,9 +0,0 @@
-#ifndef GD_RENDERER_H
-#define GD_RENDERER_H
-
-#define gd_vblank                   NULL
-#define gd_copy_p1_contpad(...)     {}
-#define gd_sfx_to_play(...)         0
-#define gdm_gettestdl(...)          NULL
-
-#endif
